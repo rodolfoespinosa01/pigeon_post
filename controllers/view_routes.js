1e530243e08fda64b6795d5501641db433fb1cd5
JD Tadlock <jt7903@gmail.com>
2023-10-16 11:15:33 -0400

    added starter comments

---
+++ controllers/view_routes.js
@@ -0,0 +1,5 @@
+// Create an express router instance object
+
+// Add one test GET route at root
+
+// Export the router
---
+++ server.js
@@ -0,0 +1,14 @@
+// Import express
+const express = require('express');
+
+// Import our view_routes
+
+// Create the port number and prepare for heroku with the process.env.PORT value
+
+// Create the server app
+
+// Open the static channel for our browser assets - ie. express.static on the public folder
+
+// Load our view routes at the root level '/'
+
+// Start the server and log the port that it started on
